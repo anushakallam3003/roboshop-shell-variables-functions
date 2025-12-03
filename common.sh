b162e316bf9e3e557dfe386154bb498779ae27bc
AnushaKallam3003 <kallamanusha67@gmail.com>
2025-12-03 18:46:55 +0530

    variables intro

--- common.sh
+++ common.sh
@@ -1,5 +1,22 @@
 color=\\e[35m
 no_color=\\e[0m
+system_setup() {
+  echo -e ${color}Restart ${component} service${no_color}
+    systemctl daemon-reload &>>/tmp/roboshop.log
+    systemctl enable ${component} &>>/tmp/roboshop.log
+    systemctl restart ${component} &>>/tmp/roboshop.log
+    echo Status - $?
+}
+
+app_prereq() {
+   echo -e ${color}Copy systemd service file${no_color}
+   cp ${component}.service /etc/systemd/system/${component}.service &>>/tmp/roboshop.log
+   echo Status - $?
+
+   echo -e ${color}Add RoboShop User${no_color}
+   useradd roboshop &>>/tmp/roboshop.log
+   echo Status - $?
+}
 
 nodejs_setup() {
   echo -e ${color}Disable default NodeJS${no_color}
@@ -14,39 +31,34 @@ nodejs_setup() {
   dnf install nodejs -y &>>/tmp/roboshop.log
   echo Status - $?
 
-  echo -e ${color}Add RoboShop User${no_color}
-  useradd roboshop &>>/tmp/roboshop.log
-  echo Status - $?
-
   echo -e ${color}Copy systemd service file${no_color}
   cp ${component}.service /etc/systemd/system/${component}.service &>>/tmp/roboshop.log
   echo Status - $?
 
-  echo -e ${color}Delete existing app content${no_color}
-  rm -rf /app &>>/tmp/roboshop.log
-  echo Status - $?
-
-  echo -e ${color}Create app directory${no_color}
-  mkdir /app &>>/tmp/roboshop.log
-  echo Status - $?
-
-  echo -e ${color}Download app code${no_color}
-  curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>/tmp/roboshop.log
-  echo Status - $?
-  cd /app
-
-  echo -e ${color}Extract app code${no_color}
-  unzip /tmp/${component}.zip &>>/tmp/roboshop.log
-  echo Status - $?
+  app_prereq
 
   cd /app
   echo -e ${color}Install NodeJS dependencies${no_color}
   npm install &>>/tmp/roboshop.log
   echo Status - $?
 
-  echo -e ${color}Restart ${component} service${no_color}
-  systemctl daemon-reload &>>/tmp/roboshop.log
-  systemctl enable ${component} &>>/tmp/roboshop.log
-  systemctl restart ${component} &>>/tmp/roboshop.log
+  systemd_setup
+}
+
+python_setup() {
+  echo -e ${color}Install Python${no_color}
+  dnf install python3 gcc python3-devel -y &>>/tmp/roboshop.log
   echo Status - $?
+
+  app_prereq
+
+  cd /app
+
+  echo -e ${color}Install Python dependencies${no_color}
+  pip3 install -r requirements.txt &>>/tmp/roboshop.log
+  echo Status - $?
+
+  systemd_setup
+
+
 }
